4a55d9c4f6ea7eaaf5d70504ed2632e553258893
Xeei <109275765+Xeei@users.noreply.github.com>
2026-06-09 08:46:22 +0700

    feat: add frontend and comment some roadmap of socket.io

--- apps/websocket/index.ts
+++ apps/websocket/index.ts
@@ -9,16 +9,22 @@ app.use(pinoHttp({logger}))
 const httpServer = createServer(app);
 const io = new Server(httpServer);
 
-
+// use for log transaction
 io.use((socket, next) => {
   socket.log = logger.child({ socketId: socket.id });
   next();
 });
 
+// this is for handle connection
 io.on("connection", (socket) => {
+	// log header of socket that connect
 	socket.log.info({ headers: socket.handshake.headers }, 'New socket connection established');
 
+
 	socket.emit("message", "Welcome to Locus Websocket "+socket.id)
+
+	// TODO: Learn how to make best pratice handle event in socket.io to be able to scale up the system
+	// TODO: Make handle event for sending coordinate of cursor
 });
 logger.info("Application start on port: "+ process.env.PORT + " http://localhost:"+process.env.PORT)
 httpServer.listen(process.env.PORT);
