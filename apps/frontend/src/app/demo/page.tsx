c365bb9c1fd1dc1e9bef54e05efb3792149b6f71
Xeei <109275765+Xeei@users.noreply.github.com>
2026-06-13 19:24:30 +0700

    chore: clean up formater

---
+++ apps/frontend/src/app/demo/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function DemoPage() {
+  return (
+    <div>
+      DemoPage
+      <div>test</div>
+    </div>
+  )
+}
---
+++ apps/frontend/src/components/Demo/DemoMap.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function DemoMap() {
+  return <div>DemoMap</div>
+}
