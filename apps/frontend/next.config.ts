085224c39a7fe62c77fad70aa15d51824d0aa3b6
Xeei <109275765+Xeei@users.noreply.github.com>
2026-06-13 19:11:24 +0700

    chore: cleanup workspace

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  transpilePackages: ["@locus/shared"]
+  transpilePackages: ["@locus/shared"],
+  turbopack: { root: path.join(__dirname, "../..")}
 };
 
 export default nextConfig;
